20c603bb669e01519c1f224e19503608a9bf2fd2
James McCormac <jmccormac001@gmail.com>
2018-01-30 00:10:05 -0300

    adding tools for checking performance

---
+++ tools/measure_rms.py
@@ -0,0 +1,12 @@
+"""
+Take a measure of the RMS from the AG log file
+
+This may be intrinsically different from the real RMS
+measured by tracking a star post-facto
+"""
+import numpy as np
+
+if __name__ == "__main__":
+    x, y = np.loadtxt('guider.log', usecols=[3, 4], unpack=True)
+    print("RMS X: {%.3f} Y: {:.3f}".format(np.std(x), np.std(y)))
+
