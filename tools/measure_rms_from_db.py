ec06700e1cfebaa86f62fb66afbe64a5af222a5d
James McCormac <jmccormac001@gmail.com>
2018-04-13 21:06:11 +0100

    adds new script for measuring RMS

---
+++ tools/measure_rms_from_db.py
@@ -0,0 +1,76 @@
+"""
+Script to measure the RMS of a given field using the database
+"""
+from collections import (
+    defaultdict,
+    OrderedDict
+    )
+import matplotlib.pyplot as plt
+import numpy as np
+import pymysql
+
+# pylint: disable=invalid-name
+
+def getUniqueObjectIds():
+    """
+    """
+    qry = """
+        SELECT
+        distinct(field)
+        FROM autoguider_ref
+        """
+    with pymysql.connect(host='localhost', db='spec_ops',
+                         user='speculoos', password='spec_ops') as cur:
+        cur.execute(qry)
+        results = cur.fetchall()
+    return results
+
+def getReferenceImagesForField(field):
+    """
+    """
+    qry = """
+        SELECT
+        ref_image, filter
+        FROM autoguider_log
+        WHERE field=%s
+        """
+    qry_args = (field, )
+    with pymysql.connect(host='localhost', db='spec_ops',
+                         user='speculoos', password='spec_ops') as cur:
+        cur.execute(qry, qry_args)
+        results = cur.fetchall()
+    ref_filts = {row[0]: row[1] for row in results}
+    return ref_filts
+
+def getAgStats(ref_image, filt):
+    """
+    """
+    qry = """
+        SELECT
+        solution_x, solution_y
+        FROM autoguider_log
+        WHERE ref_image=%s
+        AND filter=%s
+        """
+    qry_args = (ref_image, filt,)
+    with pymysql.connect(host='localhost', db='spec_ops',
+                         user='speculoos', password='spec_ops') as cur:
+        cur.execute(qry, qry_args)
+        results = cur.fetchall()
+    return results
+
+if __name__ == "__main__":
+    object_ids = getUniqueObjectIds()
+    x = OrderedDict()
+    y = OrderedDict()
+    for object_id in object_ids:
+        x[object_id] = defaultdict(list)
+        y[object_id] = defaultdict(list)
+        ref_filts = getReferenceImagesForField(object_id)
+        for ref in ref_filts:
+            stats = getAgStats(ref, ref_filts[ref])
+            for i, row in enumerate(stats):
+                # skip the first 10
+                if i >= 10:
+                    x[object_id][ref_filts[ref]].append()
+                    y[object_id][ref_filts[ref]].append()
