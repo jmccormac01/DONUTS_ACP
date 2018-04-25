a3c7c141b5232e3652da6b59b141ab21d53c3275
James McCormac <jmccormac001@gmail.com>
2018-04-25 12:22:51 +0100

    adding new touch of empty files for start/stop test

--- donuts_process.py
+++ donuts_process.py
@@ -4,6 +4,7 @@ Code to start and stop the donuts second thread
 import os
 import sys
 import argparse as ap
+from datetime import datetime
 
 # pylint: disable=invalid-name
 
@@ -19,11 +20,15 @@ def argParse():
 
 if __name__ == "__main__":
     args = argParse()
+    test_dir = "C:\\donutstests"
+    if not os.path.exists(test_dir):
+        os.mkdir(test_dir)
+    tnow = datetime.utcnow().isoformat()
     if args.action == 'start':
-        os.mkdir('C:\\testdirstart')
+        os.system('type nul >> {}\\start_{}.txt'.format(test_dir, tnow))
         sys.exit(0)
     elif args.action == 'stop':
-        os.mkdir('C:\\testdirstop')
+        os.system('type nul >> {}\\stop_{}.txt'.format(test_dir, tnow))
         sys.exit(0)
     else:
         print('Unknown input, exiting')
