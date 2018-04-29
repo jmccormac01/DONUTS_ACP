09367dbb5dba84508996bbed14447747631e2b82
James McCormac <jmccormac001@gmail.com>
2018-04-29 22:01:44 +0100

    catching all errors

--- donuts_process.py
+++ donuts_process.py
@@ -22,15 +22,16 @@ if __name__ == "__main__":
     args = argParse()
     try:
         ag = Pyro4.Proxy('PYRO:donuts@localhost:9234')
-    except:
+    except (Pyro4.errors.CommunicationError, ConnectionRefusedError):
         sys.exit(ag_status.pyro_connection_error)
-    if args.action == 'start':
+        ag = None
+    if args.action == 'start' and ag:
         status = ag.start_ag()
         sys.exit(status)
-    elif args.action == 'stop':
+    elif args.action == 'stop' and ag:
         status = ag.stop_ag()
         sys.exit(status)
-    elif args.action == 'shutdown':
+    elif args.action == 'shutdown' and ag:
         ag.shutdown()
     else:
         sys.exit(ag_status.unknown)
